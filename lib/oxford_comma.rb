c37a83f6bed100e6919205bbfaa5a9d7b1a17d78
Kyung Won Park <stomg7969@gmail.com>
2019-01-08 23:12:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.count > 1
+    array.push("and #{array.pop}")
+    
+    if array.count == 2
+      return array.join(" ")
+      
+    elsif array.count > 2
+      return array.join(", ")
+   end
+  else 
+    return array.join
+  end
 end
