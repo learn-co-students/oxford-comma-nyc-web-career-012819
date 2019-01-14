28020ca5f004d15b8b747d8b30f22f48fb24e8fe
Joshua Gluck <joshgluck1@gmail.com>
2019-01-14 05:03:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  newarray = []
+  if array.length == 2 
+    array.insert(-2," and ").join
+    elsif array.length == 1
+    array.join
+    elsif array.length > 2
+    thea = array.each { |a| print a, " " }.join(",").split(",")
+    thea[-1] = thea[-1].insert(0, "and ")
+    thea.join(", ")
 end
+end
+
