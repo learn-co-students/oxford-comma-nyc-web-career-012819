e745a84b773a358981e77854e4e89513b8cc3834
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-14 22:49:22 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    str = ''
+    for item in array do
+      if item == array[-1]
+        str << "and " + item
+      else
+        str << item + ", "
+      end
+    end
+    str
+  end
 end
