90bfab261d236b05c25c93d756d136864ac32731
Ethan Nam <330858@gmail.com>
2019-01-26 02:49:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  output = ""
+  
+  if array.size >= 3
+    output = array[0, array.size - 1].join(", ")
+    output << ", and #{array.last}"
+  elsif array.size == 2
+    output = array.join(" and ")
+  elsif array.size == 1
+    output = array[0]
+  else
+    output = ""
+  end
+  
+  output
 end
